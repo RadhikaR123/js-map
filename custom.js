8bf399a419e75ac6b742523a7087a08ec42bd64e
RadhikaR123 <103849462+RadhikaR123@users.noreply.github.com>
2022-04-17 23:08:48 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,15 @@
+// const string = "JavaScript";
+// const new_string=string.split('');
+
+// let asciar=new_string.map((x)=> x.charCodeAt(0));
+// console.log(asciar)
+
+
+
+
+function ar(num){
+    console.log("hi",this.name)
+}
+ar({name:"radhika"})
+
+
---
+++ sqrt.js
@@ -0,0 +1,4 @@
+const prices = [1800, 2000, 3000, 5000, 500, 8000];
+
+let sqrt=prices.map(Math.sqrt);
+console.log(sqrt)
